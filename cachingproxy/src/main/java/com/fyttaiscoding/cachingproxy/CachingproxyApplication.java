eb1ecd4ecd8ffc225fe396b8a97237403bdbf0f3
fytta <fytta@fytta-msi.fyttaiscoding.com>
2025-04-24 21:26:46 +0200

    first commit

---
+++ cachingproxy/src/main/java/com/fyttaiscoding/cachingproxy/CachingproxyApplication.java
@@ -0,0 +1,13 @@
+package com.fyttaiscoding.cachingproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingproxyApplication.class, args);
+	}
+
+}
---
+++ cachingproxy/src/test/java/com/fyttaiscoding/cachingproxy/CachingproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyttaiscoding.cachingproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
